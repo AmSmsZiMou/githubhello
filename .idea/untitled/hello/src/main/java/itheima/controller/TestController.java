a20875db6fbf175443a4d341b57821c605688193
zimou <547593609@qq.com>
2023-01-11 18:37:57 +0800

    无

--- .idea/untitled/hello/src/main/java/itheima/controller/TestController.java
+++ .idea/untitled/hello/src/main/java/itheima/controller/TestController.java
@@ -1,8 +1,11 @@
 package itheima.controller;
 
+import javax.annotation.Resource;
+
 /**
  * @author shkstart
  * @create 2023--01--2023/1/11--18:01
  */
+@Resource
 public class TestController {
 }
