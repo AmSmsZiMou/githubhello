e1c83aecfd63bcc5490b463014f6e8eace00ba04
zimou <547593609@qq.com>
2023-01-11 21:18:08 +0800

    无

---
+++ .idea/untitled/hello/src/main/java/itheima/controller/TestController2.java
@@ -0,0 +1,13 @@
+package itheima.controller;
+
+/**
+ * @author shkstart
+ * @create 2023--01--2023/1/11--21:16
+ */
+public class TestController2 {
+    public static void main(String[] args) {
+        System.out.println("脑残");
+    }
+
+}
+
